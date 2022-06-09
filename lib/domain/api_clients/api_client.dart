74532cc1d61ee0702fd4bc2954e7c83b6749fe89
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-09 15:32:07 +0300

    finished http request

--- lib/domain/api_clients/api_client.dart
+++ lib/domain/api_clients/api_client.dart
@@ -6,16 +6,24 @@ import 'package:flutter_http_request/domain/entity/post.dart';
 class ApiClient {
   final client = HttpClient();
 
-  void getPosts() async {
-    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-    //Uri(scheme: 'https', host: 'https://jsonplaceholder.typicode.com', path: 'posts',); Second way to getPost
-    final request = await client.getUrl(url);
-    final response = await request.close();
-    final jsonStrings = await response.transform(utf8.decoder).toList();
-    final jsonString = jsonStrings.join();
-    final json = jsonDecode(jsonString) as List<dynamic>;
+  Future<List<Post>> getPosts() async {
+    final json = await get('https://jsonplaceholder.typicode.com/posts')
+        as List<dynamic>;
+
     final posts = json
         .map((dynamic e) => Post.fromJson(e as Map<String, dynamic>))
         .toList();
+    return posts;
+  }
+
+  Future<dynamic> get(String ulr) async {
+    final url = Uri.parse(ulr);
+    final request = await client.getUrl(url);
+    final response = await request.close();
+
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+    final dynamic json = jsonDecode(jsonString);
+    return json;
   }
 }
---
+++ lib/example/example_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_http_request/domain/api_clients/api_client.dart';
+import 'package:flutter_http_request/domain/entity/post.dart';
+
+class ExampleWidgetModel extends ChangeNotifier {
+  final apiClient = ApiClient();
+  var _posts = <Post>[];
+  List<Post> get posts => _posts;
+
+  Future<void> reloadPosts() async {
+    final posts = await apiClient.getPosts();
+    _posts += posts;
+    notifyListeners();
+  }
+
+  void createPost() {}
+}
+
+class ExampleModelProvider extends InheritedNotifier {
+  final ExampleWidgetModel model;
+  const ExampleModelProvider({
+    Key? key,
+    required this.model,
+    required Widget child,
+  }) : super(
+          key: key,
+          notifier: model,
+          child: child,
+        );
+
+  static ExampleModelProvider? watch(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ExampleModelProvider>();
+  }
+
+  static ExampleModelProvider? read(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<ExampleModelProvider>()
+        ?.widget;
+    return widget is ExampleModelProvider ? widget : null;
+  }
+}
