5f73cd40667ceb5357b8819377778024af9154c3
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-09 15:54:09 +0300

    method post started but not finished

--- lib/domain/api_clients/api_client.dart
+++ lib/domain/api_clients/api_client.dart
@@ -16,6 +16,21 @@ class ApiClient {
     return posts;
   }
 
+  Future<Post> createPost({required String title, required String body}) async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final parameters = <String, dynamic>{
+      'title': title,
+      'body': body,
+      'userId': 109
+    };
+    final request = await client.postUrl(url);
+    request.headers.set('Content-type', 'application/json; charset=UTF-8');
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+  }
+
   Future<dynamic> get(String ulr) async {
     final url = Uri.parse(ulr);
     final request = await client.getUrl(url);
